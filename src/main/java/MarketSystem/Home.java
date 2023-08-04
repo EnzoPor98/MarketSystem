59af29d21babf431733cf6a8dd57f81efba9316c
Usuario <Usuario@PC-Gamer>
2023-08-04 15:06:40 -0300

    HomeFinish

---
+++ src/main/java/MarketSystem/Home.java
@@ -0,0 +1,268 @@
+package MarketSystem;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.swing.JOptionPane;
+
+/**
+ * @author Enzo Portillo
+ */
+
+public class Home extends javax.swing.JFrame {
+    public Home() {
+        initComponents();
+        setLocationRelativeTo(null);
+    }
+
+    //--------------------------------------------------------------------------
+    //--------------------------------------------------------------------------
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jLabel1 = new javax.swing.JLabel();
+        jPanel1 = new javax.swing.JPanel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        passField = new javax.swing.JPasswordField();
+        jLabel5 = new javax.swing.JLabel();
+        quitButton = new javax.swing.JButton();
+        inButton = new javax.swing.JButton();
+        jLabel7 = new javax.swing.JLabel();
+        userField = new javax.swing.JTextField();
+        jSeparator1 = new javax.swing.JSeparator();
+        jLabel6 = new javax.swing.JLabel();
+        jLabel8 = new javax.swing.JLabel();
+        jLabel9 = new javax.swing.JLabel();
+        jLabel10 = new javax.swing.JLabel();
+        jLabel11 = new javax.swing.JLabel();
+        jLabel12 = new javax.swing.JLabel();
+        jLabel13 = new javax.swing.JLabel();
+        jLabel15 = new javax.swing.JLabel();
+        jLabel16 = new javax.swing.JLabel();
+        jLabel18 = new javax.swing.JLabel();
+        jLabel19 = new javax.swing.JLabel();
+        jLabel20 = new javax.swing.JLabel();
+        jLabel21 = new javax.swing.JLabel();
+        jLabel14 = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setBackground(new java.awt.Color(255, 255, 255));
+        setUndecorated(true);
+        setResizable(false);
+
+        jLabel1.setIcon(new javax.swing.ImageIcon("C:\\Users\\Usuario\\OneDrive\\Escritorio\\Programación\\Proyectos Personales\\MarketSystem\\Images\\Home.jpg")); // NOI18N
+
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel2.setIcon(new javax.swing.ImageIcon("C:\\Users\\Usuario\\OneDrive\\Escritorio\\Programación\\Proyectos Personales\\MarketSystem\\Images\\User.png")); // NOI18N
+        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(570, 10, 220, 140));
+
+        jLabel3.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        jLabel3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel3.setText("Usuario:");
+        jLabel3.setPreferredSize(new java.awt.Dimension(78, 20));
+        jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(580, 190, -1, 20));
+
+        jLabel4.setFont(new java.awt.Font("Arial", 1, 12)); // NOI18N
+        jLabel4.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel4.setText("Enzo Portillo | Derechos Reservados ©");
+        jPanel1.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 320, 790, -1));
+
+        passField.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        passField.setHorizontalAlignment(javax.swing.JTextField.CENTER);
+        jPanel1.add(passField, new org.netbeans.lib.awtextra.AbsoluteConstraints(680, 230, 110, -1));
+
+        jLabel5.setFont(new java.awt.Font("Arial", 0, 12)); // NOI18N
+        jLabel5.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel5.setText("Con este programa podrá gestionar su negocio de manera eficiente. MarketSystem incluye...");
+        jPanel1.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 50, 530, -1));
+
+        quitButton.setFont(new java.awt.Font("Arial", 1, 12)); // NOI18N
+        quitButton.setText("SALIR");
+        quitButton.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        quitButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                quitButtonActionPerformed(evt);
+            }
+        });
+        jPanel1.add(quitButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(710, 270, 70, 40));
+
+        inButton.setFont(new java.awt.Font("Arial", 1, 12)); // NOI18N
+        inButton.setText("INGRESAR");
+        inButton.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        inButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                inButtonActionPerformed(evt);
+            }
+        });
+        jPanel1.add(inButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(590, 270, -1, 40));
+
+        jLabel7.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        jLabel7.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel7.setText("Contraseña:");
+        jLabel7.setPreferredSize(new java.awt.Dimension(78, 20));
+        jPanel1.add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(580, 230, -1, 20));
+
+        userField.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        userField.setHorizontalAlignment(javax.swing.JTextField.CENTER);
+        jPanel1.add(userField, new org.netbeans.lib.awtextra.AbsoluteConstraints(680, 190, 110, -1));
+
+        jSeparator1.setBackground(new java.awt.Color(255, 255, 255));
+        jSeparator1.setForeground(new java.awt.Color(0, 0, 0));
+        jSeparator1.setOrientation(javax.swing.SwingConstants.VERTICAL);
+        jPanel1.add(jSeparator1, new org.netbeans.lib.awtextra.AbsoluteConstraints(550, 20, 10, 290));
+
+        jLabel6.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
+        jLabel6.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel6.setText("¡ BIENVENIDO A MARKET SYSTEM !");
+        jPanel1.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 10, 530, -1));
+
+        jLabel8.setFont(new java.awt.Font("Arial", 0, 12)); // NOI18N
+        jLabel8.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel8.setText("Inicie sesión para ingresar al sistema...");
+        jPanel1.add(jLabel8, new org.netbeans.lib.awtextra.AbsoluteConstraints(570, 160, 220, -1));
+
+        jLabel9.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        jLabel9.setText(" manualmente.");
+        jPanel1.add(jLabel9, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 300, 510, -1));
+
+        jLabel10.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        jLabel10.setText(">> MODO ADMINISTRADOR: puede gestionar usuarios, productos y ver las ventas.");
+        jPanel1.add(jLabel10, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 80, 530, -1));
+
+        jLabel11.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        jLabel11.setText(">> MODO EMPLEADO: puede realizar las ventas, ver los productos y las ventas.");
+        jPanel1.add(jLabel11, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 100, 530, -1));
+
+        jLabel12.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        jLabel12.setText(">> GENERADOR TXT: genera un archivo de texto con las ventas realizadas del día");
+        jPanel1.add(jLabel12, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 240, 530, -1));
+
+        jLabel13.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        jLabel13.setText(">> SISTEMA DE TURNOS: al iniciar sesión, se comienza con el turno de forma");
+        jPanel1.add(jLabel13, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 180, 530, -1));
+
+        jLabel15.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        jLabel15.setText("cada vez que se cierra un turno. Esto se realiza con el fin de almacenar la");
+        jPanel1.add(jLabel15, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 260, 510, -1));
+
+        jLabel16.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        jLabel16.setText(">> BASE DE DATOS: almacena usuarios y productos, además de las ventas que se");
+        jPanel1.add(jLabel16, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 120, 530, -1));
+
+        jLabel18.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        jLabel18.setText("automática. Al cerrar sesión, se cierra el turno y se muestra el horario de");
+        jPanel1.add(jLabel18, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 200, 510, -1));
+
+        jLabel19.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        jLabel19.setText("apertura y cierre del mismo, además del monto total recaudado.");
+        jPanel1.add(jLabel19, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 220, 510, -1));
+
+        jLabel20.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        jLabel20.setText("hicieron hasta hace un año atrás. Luego se eliminan automáticamente para");
+        jPanel1.add(jLabel20, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 140, 510, -1));
+
+        jLabel21.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        jLabel21.setText("ahorrar espacio de almacenamiento.");
+        jPanel1.add(jLabel21, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 160, 510, -1));
+
+        jLabel14.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        jLabel14.setText("información de manera indefinida, hasta que el administrador desee eliminarla");
+        jPanel1.add(jLabel14, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 280, 510, -1));
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jLabel1)
+                .addGap(0, 0, Short.MAX_VALUE))
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel1))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+    //--------------------------------------------------------------------------
+    //--------------------------------------------------------------------------
+    
+    //----------------------------------------------------------- LOG IN BUTTON.
+    private void inButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_inButtonActionPerformed
+        try{
+            Connection cn = DriverManager.getConnection("jdbc:mysql://localhost:3306/marketsystem","root","");
+            PreparedStatement pst = cn.prepareStatement("select usuario, contraseña, estado from usuarios");
+            ResultSet rs = pst.executeQuery();
+            
+            if(rs.next()){
+                if(rs.getString("usuario").equals(userField.getText()) &&
+                   rs.getString("contraseña").equals(passField.getText()) &&
+                   rs.getString("estado").equals("Activo")){
+                    this.setVisible(false);
+                    new Manager().setVisible(true);
+                } else if(rs.getString("estado").equals("Inactivo")){
+                    JOptionPane.showMessageDialog(null, "Lo sentimos, usted no se encuentra activo en la base de datos.");
+                } else{
+                    JOptionPane.showMessageDialog(null, "Usuario y/o contraseña incorrectos, intente nuevamente.");
+                }
+            }
+        } catch(SQLException e){
+            JOptionPane.showMessageDialog(null, "No se ha podido establecer conexión con la base de datos.\n"+e);
+        }
+    }//GEN-LAST:event_inButtonActionPerformed
+
+    //--------------------------------------------------------- SIGN OFF BUTTON.
+    private void quitButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_quitButtonActionPerformed
+        System.exit(0);
+    }//GEN-LAST:event_quitButtonActionPerformed
+    
+    //--------------------------------------------------------------------------
+    public static void main(String args[]) {
+        java.awt.EventQueue.invokeLater(() -> {
+            new Home().setVisible(true);
+        });
+    }
+    //--------------------------------------------------------------------------
+    
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton inButton;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel10;
+    private javax.swing.JLabel jLabel11;
+    private javax.swing.JLabel jLabel12;
+    private javax.swing.JLabel jLabel13;
+    private javax.swing.JLabel jLabel14;
+    private javax.swing.JLabel jLabel15;
+    private javax.swing.JLabel jLabel16;
+    private javax.swing.JLabel jLabel18;
+    private javax.swing.JLabel jLabel19;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel20;
+    private javax.swing.JLabel jLabel21;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
+    private javax.swing.JLabel jLabel9;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JSeparator jSeparator1;
+    private javax.swing.JPasswordField passField;
+    private javax.swing.JButton quitButton;
+    private javax.swing.JTextField userField;
+    // End of variables declaration//GEN-END:variables
+}
